59ba24c304f4c9d44784946578cc91ced1fcf003
N. Medvedev <n.a.medvedev@gmail.com>
2024-04-17 07:30:49 +0200

    Update Dealing_with_output_files.f90

--- Source_files/Dealing_with_output_files.f90
+++ Source_files/Dealing_with_output_files.f90
@@ -47,7 +47,7 @@ use Dealing_with_CDF, only : write_CDF_file
 implicit none
 PRIVATE
 
-character(30), parameter :: m_XTANT_version = 'XTANT-3 (version 15.04.2024)'
+character(30), parameter :: m_XTANT_version = 'XTANT-3 (version 17.04.2024)'
 character(30), parameter :: m_Error_log_file = 'OUTPUT_Error_log.txt'
 
 public :: write_output_files, convolve_output, reset_dt, print_title, prepare_output_files, communicate
